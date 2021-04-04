5cde9afb48b5ed32db911c61e242184597146db3
ahmedtariq11 <70329164+ahmedtariq11@users.noreply.github.com>
2021-04-04 22:44:00 +0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -175,6 +175,10 @@ function exclude_woocommerce_styles() {
 //remove generator meta tag
 remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
 
+function exclude_woocommerce_styles() {
+//remove generator meta tag
+remove_action( 'wp_head', array( $GLOBALS['woocommerce'], 'generator' ) );
+
 //check woo exists
 if ( function_exists( 'is_woocommerce' ) ) {
 //dequeue scripts and styles
@@ -199,6 +203,35 @@ if ( function_exists( 'is_woocommerce' ) ) {
     wp_dequeue_script( 'jquery-placeholder' );
     wp_dequeue_script( 'fancybox' );
     wp_dequeue_script( 'jqueryui' );
+    
+    /* YESTE WE ADD */
+     wp_dequeue_script( 'wc-add-payment-method' );
+ wp_dequeue_script( 'wc-lost-password' );
+ wp_dequeue_script( 'wc_price_slider' );
+ wp_dequeue_script( 'wc-single-product' );
+ wp_dequeue_script( 'wc-add-to-cart' );
+ wp_dequeue_script( 'wc-cart-fragments' );
+ wp_dequeue_script( 'wc-credit-card-form' );
+ wp_dequeue_script( 'wc-checkout' );
+
+
+ wp_dequeue_style( 'woocommerce-smallscreen' );
+ wp_dequeue_style( 'woocommerce_fancybox_styles' );
+ wp_dequeue_style( 'woocommerce_chosen_styles' );
+ wp_dequeue_style( 'woocommerce_prettyPhoto_css' );
+ wp_dequeue_style('wc-memberships-frontend');
+ wp_dequeue_style('membership-metaboxes');
+ wp_dequeue_style('membership-frontendcss');
+ wp_dequeue_style('membership-subscriptionformcss');
+ wp_dequeue_style('membership-accountformcss');
+ wp_dequeue_style('membership-subscriptionformcss');
+ wp_dequeue_style('membership-upgradeformcss');
+ wp_dequeue_style('membership-renewformcss');
+ wp_dequeue_style('membership-fancyboxcss');
+ wp_dequeue_style('membership-popupmemcss');
+ wp_dequeue_style('membership-accountformcss');
+ wp_dequeue_style('membership-upgradeformcss');
+ wp_dequeue_style('membership-renew');
     }
   }
 }
